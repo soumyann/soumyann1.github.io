ae17ff19932f6f19fe9313eb836ea788c95f71e7
SNN <soumya.nn@hotmail.com>
2022-01-21 23:57:44 -0500

    final commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,11 +8,47 @@
         let AboutUsButton = document.getElementById("AboutUsButton");
         AboutUsButton.addEventListener("click", function()
         {
+            //redirect to about page
             location.href = "about.html";
 
-        })        
-    }
+        });
+        
+        // Step 1 get a reference to an entry point(s) (insertion point/)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+        
+        // Step 2 create an element to insert
+        let MainParagraph = document.createElement("p");
+        let Article=document.createElement("article");
+        let ArticleParagraph=`<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph`;
 
+        // Step 3 configure new element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        let FirstString = "This is";
+        let SecondString = `${FirstString} the Main Paragraph`;
+        MainParagraph.textContent = SecondString;
+        Article.setAttribute("class", "container");
+
+        // Step 4 add/insert new element
+        // exaple of insert after (append)
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML=ArticleParagraph;
+        DocumentBody.appendChild(Article);
+        // DocumentBody.innerHTML = `
+        // <div class="container">
+        // <h1 class="display-1">Hello, World!</h1>
+        // <p clas="mt-5">and...whato do you think of this!</p>
+        // </div>
+        // `;
+        //example of insert before
+        //MainContent.before(MainParagraph);
+        //example of deletion
+        //document.getElementById("AboutUsButton").remove();
+        //AboutUsButton.remove();
+        // ES6 and HTML5
+    }
+/* 
     function DisplayProductsPage()
     {
 
@@ -28,7 +64,7 @@
     function DisplayContactPage()
     {
 
-    }
+    } */
     
     // named function option
     function Start()
@@ -39,7 +75,7 @@
             case "Home":
                 DisplayHomePage();
                 break;
-            case "Our Products":
+            /* case "Our Products":
                 DisplayProductsPage();
                 break;
             case "Our Services":
@@ -50,13 +86,10 @@
                 break;
             case "Contact Us":
                 DisplayContactPage();
-                break;
+                break; */
         }        
     }
-    /* let Start = function()
-    {
-        console.log("App Started");
-    } */
+    
     
     window.addEventListener("load", Start);
 
